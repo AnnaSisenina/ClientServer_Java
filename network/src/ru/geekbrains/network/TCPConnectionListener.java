06cd0deadb49a22d92148d5769c2f4dda8c4a815
Anna Sisenina <yuta4ka71@gmail.com>
2023-07-04 23:39:24 +0300

    ServerClien added

---
+++ network/src/ru/geekbrains/network/TCPConnectionListener.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.network;
+
+public interface TCPConnectionListener {
+
+    void onConnectionRead(TCPConnection tcpConnection);
+    void onReceiveString(TCPConnection tcpConnection, String value);
+    void onDisconnect (TCPConnection tcpConnection);
+    void onException (TCPConnection tcpConnection, Exception e);
+}
